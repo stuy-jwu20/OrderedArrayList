57babfc055edf8e93e5c5fe7d107f3b8cc38f0ab
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 15:40:27 -0500

    fixed bugs by added compareTo method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 
-  public OrderedArrayList<T>() {
+  public OrderedArrayList() {
     super();
   }
 
-  public OrderedArrayList<T>(int startingCapacity) {
+  public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
   public boolean add(T value) {
     if (this.size() == 0) this.add(value);
-    for (int i = 0; this.size > 1 && i < this.size() - 1; i++) {
-      if (this.get(i) < value) && (value < this.get(i+1)) {
+    for (int i = 0; this.size() > 1 && i < this.size() - 1; i++) {
+      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0)) {
         this.add(i+1, value);
         return true;
       }
-      else if (value < this.get(0)) {
+      else if (value.compareTo(this.get(0)) < 0) {
         this.add(0, value);
       }
     }
@@ -24,7 +24,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public void add(int index, T value) {
     for (int i = 0; i < this.size() - 1; i++) {
-      if (this.get(i) < value) && (value < this.get(i+1)) {
+      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0))  {
         this.add(i+1, value);
       }
     }
