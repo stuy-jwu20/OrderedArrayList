2a9b2a9a0d3a40dadceebe021789f34432c599a1
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 14:42:49 -0500

    gave return types to all methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T> {
+
   public T set(int index, T value) {
     if (index < 0 || index >= this.size()) {
       throw new IndexOutOfBoundsException("\n ! Your index is out of bounds! It should be at least 0 and less than " + size() + " but we received this from you: " + index + " !");
@@ -7,14 +8,16 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     else if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
+    T old = this.get(index);
     this.set(index, value);
+    return old;
   }
 
   public boolean add(T value) {
     if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
-    this.add(value);
+    return this.add(value);;
   }
 
   public boolean add(int index, T value) {
@@ -24,6 +27,6 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     else if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
-    this.add(index, value);
+    return this.add(index, value);
   }
 }
