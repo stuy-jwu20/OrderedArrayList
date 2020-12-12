3a5ec46b7c42ac7579a6329f2e5f85875f35582a
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 14:39:43 -0500

    overrided all methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,27 +1,29 @@
 import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T> {
   public T set(int index, T value) {
-    if (index < 0 || index >= size()) {
+    if (index < 0 || index >= this.size()) {
       throw new IndexOutOfBoundsException("\n ! Your index is out of bounds! It should be at least 0 and less than " + size() + " but we received this from you: " + index + " !");
     }
     else if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
-
+    this.set(index, value);
   }
 
   public boolean add(T value) {
     if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
+    this.add(value);
   }
 
   public boolean add(int index, T value) {
-    if (index < 0 || index >= size()) {
+    if (index < 0 || index >= this.size()) {
       throw new IndexOutOfBoundsException("\n ! Your index is out of bounds! It should be at least 0 and less than " + size() + " but we received this from you: " + index + " !");
     }
     else if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
+    this.add(index, value);
   }
 }
