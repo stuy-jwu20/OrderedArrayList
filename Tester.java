e966c4b9f2362f47c275dbf2dc4cf126e3674e96
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 17:12:30 -0500

    mini tester addition

---
+++ Tester.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Tester {
+  public static void main(String[] args) {
+
+// NoNullArrayList Mini Tester//
+    NoNullArrayList<String> noNull = new NoNullArrayList<String>();
+    NoNullArrayList<String> noNull2 = new NoNullArrayList<String>(20);
+    String letter = "a";
+    for (int i = 0; i < 12; i++) {
+      letter += "a";
+      noNull2.add(0, letter);
+    }
+    for (int i = 0; i < 6; i++) {
+      letter += "a";
+      noNull.add(letter);
+    }
+    noNull2.add(2, "newWord");
+    noNull2.add("");
+    //noNull2.add(null);
+    noNull.add(2, "newWord");
+    noNull.add("");
+    //noNull.add(null);
+
+//OrderedArrayList Mini Tester//
+    OrderedArrayList<Integer> ordered = new OrderedArrayList<Integer>();
+    OrderedArrayList<Integer> ordered2 = new OrderedArrayList<Integer>(20);
+    int num = 1000;
+    for (int i = 0; i < 15; i++) {
+      ordered2.add(num);
+      num -= 100;
+    }
+    for (int i = 0; i < 6; i++) {
+      ordered.add(num);
+      num -= 100;
+    }
+    ordered2.add(7, 99);
+    System.out.println(ordered2);
+    ordered2.set(3, 666);
+    System.out.println(ordered2);
+  }
+}
