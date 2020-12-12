93f3bb3520270e1ffafbbd80bf33bc425bb0f8cc
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 15:03:31 -0500

    made add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,4 +1,13 @@
 import java.util.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
-  
+
+  public boolean add(T value) {
+    for (int i = 0; i < this.size() - 1; i++) {
+      if (this.get(i) < value) && (value < this.get(i+1)) {
+        this.add(i+1, value);
+        return true;
+      }
+    }
+    return false;
+  }
 }
