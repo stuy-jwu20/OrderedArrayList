ccc99b43cd5088a684c284101feb1adbf58f12f3
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 15:25:52 -0500

    removed a few exceptions

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -10,10 +10,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public T set(int index, T value) {
-    if (index < 0 || index >= this.size()) {
-      throw new IndexOutOfBoundsException("\n ! Your index is out of bounds! It should be at least 0 and less than " + size() + " but we received this from you: " + index + " !");
-    }
-    else if (value == null) {
+    if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
     T old = this.get(index);
@@ -29,10 +26,7 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   public void add(int index, T value) {
-    if (index < 0 || index >= this.size()) {
-      throw new IndexOutOfBoundsException("\n ! Your index is out of bounds! It should be at least 0 and less than " + size() + " but we received this from you: " + index + " !");
-    }
-    else if (value == null) {
+    if (value == null) {
       throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
     this.add(index, value);
