f7b29c841396a27e885608f400db64824bd772fc
Jonathan Wu <jwu20@stuy.edu>
2020-12-12 17:10:47 -0500

    fixed bugs and made it work properly

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,31 +9,49 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
   public boolean add(T value) {
-    if (this.size() == 0) this.add(value);
+    if (this.size() == 0) {
+      super.add(value);
+      return true;
+    }
+    else if (this.size() == 1) {
+      if (value.compareTo(this.get(0)) < 0) {
+        super.add(0, value);
+        return true;
+      }
+    }
     for (int i = 0; this.size() > 1 && i < this.size() - 1; i++) {
       if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0)) {
-        this.add(i+1, value);
+        super.add(i, value);
         return true;
       }
       else if (value.compareTo(this.get(0)) < 0) {
-        this.add(0, value);
+        super.add(0, value);
+        return true;
       }
     }
     return false;
   }
 
   public void add(int index, T value) {
+    int once = 0;
     for (int i = 0; i < this.size() - 1; i++) {
-      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0))  {
-        this.add(i+1, value);
+      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0) & once == 0)  {
+        super.add(i, value);
+        once++;
       }
     }
   }
 
   public T set(int index, T value) {
-    T old = this.get(index);
-    this.remove(index);
-    add(value);
+    T old = super.get(index);
+    super.remove(index);
+    int once = 0;
+    for (int i = 0; i < this.size() - 1; i++) {
+      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) < 0) && once == 0) {
+        super.add(i, value);
+        once++;
+      }
+    }
     return old;
   }
 }
