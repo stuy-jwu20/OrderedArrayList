d4ea27001e27316d4d5be4893974f472604a4605
Jonathan Wu <jwu20@stuy.edu>
2020-12-13 23:26:12 -0500

    cleaned up code and made it more efficient

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,50 +8,31 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
   public boolean add(T value) {
-    if (this.size() == 0) {
-      super.add(value);
-      return true;
+    if (value == null) {
+      throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
-    else if (this.size() == 1) {
-      if (value.compareTo(this.get(0)) < 0) {
-        super.add(0, value);
-        return true;
-      }
-    }
-    for (int i = 0; this.size() > 1 && i < this.size() - 1; i++) {
-      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) <= 0)) {
+    int sizeNow = this.size();
+    for (int i = 0; i < sizeNow; i++) {
+      if (value.compareTo(this.get(i)) <= 0) {
         super.add(i, value);
         return true;
       }
-      else if (value.compareTo(this.get(0)) < 0) {
-        super.add(0, value);
-        return true;
-      }
     }
-    return false;
+    return super.add(value);
   }
 
   public void add(int index, T value) {
-    int once = 0;
-    for (int i = 0; i < this.size() - 1; i++) {
-      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) <= 0) & once == 0)  {
-        super.add(i, value);
-        once++;
-      }
-    }
+    this.add(value);
   }
 
   public T set(int index, T value) {
-    T old = super.get(index);
-    super.remove(index);
-    int once = 0;
-    for (int i = 0; i < this.size() - 1; i++) {
-      if ((this.get(i).compareTo(value) > 0) && (value.compareTo(this.get(i+1)) <= 0) && once == 0) {
-        super.add(i, value);
-        once++;
-      }
+    if (value == null) {
+      throw new IllegalArgumentException("You tried to set a null value! That is not allowed.");
     }
+    T old = this.remove(index);
+    this.add(value);
     return old;
   }
 }
